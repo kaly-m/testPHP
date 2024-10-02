ef8afb101ae5d26c105bf880502a2f403b131224
Michiel Kelian <kelian.michiel@etu.umontpellier.fr>
2024-10-02 20:41:10 +0200

    ajout ligne index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 $date = new \DateTime();
 
 echo "<p>Hello world!</p>";
-
+echo "<p>Nous sommes le <strong>{$date->format('j F Y')}</strong> et il est <strong>{$date->format('H:i')}</strong></p>";
 ?>
